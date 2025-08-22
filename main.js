804a2b326d11329195ff87dfbb9fb16e672f20c9
s0901598 <s0901598@gmail.com>
2025-08-22 10:58:53 +0800

    更新html

--- main.js
+++ main.js
@@ -37,8 +37,6 @@ function reveal(){
 }
 window.addEventListener("scroll",reveal)
 
-
-
 function showgallery(id){
     document.getElementById(id).style.display = "block";
     document.getElementById("gallery").style.display = "block";
@@ -72,47 +70,17 @@ function closegallery(){
 }
 
 
-//form
-// function AttendFunction(){
-//     let Form1 = document.getElementById('form1'),
-//     notAttend = Form1.querySelectorAll(".not-attend"),
-//     ATTEND = Form1["ATTEND"],
-//     ADULT = Form1["ADULT"],
-//     KID = Form1["KID"],
-//     VEGAN = Form1["VEGAN"];
-
-
-//     if ( ATTEND.value == "出席" ){  
-//         ADULT.value = "";
-//         KID.value = "";
-//         VEGAN.value = "";
-
-//         notAttend.forEach(noreply => {
-//             noreply.style.display = "flex";
-//         });
-//     } else {
-//         ADULT.value = "0";
-//         KID.value = "0";
-//         VEGAN.value = "0";
-
-//         notAttend.forEach(noreply => {
-//             noreply.style.display = "none";
-//         });
-//     };
-
-// }
-
 function AttendFunction(){
     let Form1 = document.getElementById('form1'),
     notAttend = Form1.querySelectorAll(".not-attend"),
-    ATTEND = Form1["entry.1130099705"],
-    ADULT = Form1["entry.1282042653"],
-    KID = Form1["entry.401083884"],
-    VEGAN = Form1["entry.1580275973"],
-    INVITATION = Form1["entry.185161345"],
-    ADDRESS = Form1["entry.1677057149"],
+    ATTEND = Form1["entry.877657756"],
+    ADULT = Form1["entry.1186257173"],
+    KID = Form1["entry.168231074"],
+    VEGAN = Form1["entry.1236172646"],
+    INVITATION = Form1["entry.830037336"],
+    ADDRESS = Form1["entry.1934957082"],
     NAME = Form1["entry.332417739"],
-    MESSAGE = Form1["entry.1360828809"];
+    MESSAGE = Form1["entry.1404975391"];
 
     // 清空所有欄位
     NAME.value = "";
@@ -124,7 +92,11 @@ function AttendFunction(){
     MESSAGE.value = "";
     ADDRESS.disabled = false; // 確保地址欄位恢復可編輯
 
-    if ( ATTEND.value == "出席" ) {  
+    if ( ATTEND.value == "出席" ){  
+        ADULT.value = "";
+        KID.value = "";
+        VEGAN.value = "";
+
         notAttend.forEach(noreply => {
             noreply.style.display = "flex";
         });
@@ -134,44 +106,16 @@ function AttendFunction(){
         VEGAN.value = "0";
         notAttend.forEach(noreply => {
             noreply.style.display = "none";
-        });
-    }
+        }  
+        );
+        
+    };
 }
-
-// function InvitationFunction(){
-//     let Form1 = document.getElementById('form1'),
-//     INVITATION = Form1["INVITATION"],
-//     ADDRESS = Form1["ADDRESS"];
-
-
-//     if ( INVITATION.value == "需要" || INVITATION.value == "" ){  
-//         ADDRESS.value = "";
-//         ADDRESS.disabled = false;
-//     } else {
-//         ADDRESS.value = "X";
-//         ADDRESS.disabled = true;
-//     };
-
-// }
-
-// function InvitationFunction(){
-//     let Form1 = document.getElementById('form1'),
-//     INVITATION = Form1["entry.1515955774"],
-//     ADDRESS = Form1["entry.1246916581"];
-
-//     if ( INVITATION.value == "不需要" ) {  
-//         ADDRESS.value = "X";
-//         ADDRESS.disabled = true;
-//     } else {
-//         ADDRESS.value = "";
-//         ADDRESS.disabled = false;
-//     }
-// }
 
 function InvitationFunction(){
     let Form1 = document.getElementById('form1'),
-    INVITATION = Form1["entry.185161345"],
-    ADDRESS = Form1["entry.1677057149"];
+    INVITATION = Form1["entry.830037336"],
+    ADDRESS = Form1["entry.1934957082"];
 
     if ( INVITATION.value == "不需要" ) {  
         ADDRESS.value = "X";
@@ -182,50 +126,8 @@ function InvitationFunction(){
     }
 }
 
-// Add this at the top of main.js
-var submitted = false;
 
 // Existing AttendFunction and InvitationFunction remain the same
-
-// function submitform1(){
-//     let Form1 = document.getElementById('form1'),
-//     NAME = document.getElementById("Name").value,
-//     ATTEND = Form1.elements["entry.1130099705"].value,
-//     ADULT = document.getElementById("Adult").value,
-//     KID = document.getElementById("Kid").value,
-//     VEGAN = document.getElementById("Vegan").value,
-//     INVITATION = document.getElementById("Invitation").value,
-//     ADDRESS = document.getElementById("Address").value,
-//     MESSAGE = document.getElementById("Message").value;
-
-//     if( NAME === "" ){
-//         document.getElementById('Name').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請填寫姓名');
-//     } else if( ADULT === "" ){
-//         document.getElementById('Adult').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請回答大人人數');
-//     } else if( KID === "" ){
-//         document.getElementById('Kid').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請回答小孩人數');
-//     } else if( VEGAN === "" ){
-//         document.getElementById('Vegan').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請問是否有素食需求');
-//     } else if( INVITATION === "" ){
-//         document.getElementById('Invitation').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請問是否需要紙本喜帖');
-//     } else if( ADDRESS === "" ){
-//         document.getElementById('Address').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-//         alert('請留下喜帖收件地址');
-//     } else{
-//         // Optional: If you want SUBMITTIME, add a hidden input to the form like <input type="hidden" id="SUBMITTIME" name="entry.XXXXX"> and set it here
-//         // For now, assuming not needed
-
-//         // document.getElementById('loading').style.display = "block"; // If you have loading, keep; else remove
-
-//         submitted = true;
-//         Form1.submit();
-//     }
-// }  
 
 function submitform1(){
     let Form1 = document.getElementById('form1'),
@@ -237,83 +139,39 @@ function submitform1(){
     INVITATION = document.getElementById("Invitation").value,
     ADDRESS = document.getElementById("Address").value,
     MESSAGE = document.getElementById("Message").value;
-
-    if( NAME === "" ){
-        document.getElementById('Name').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請填寫姓名');
-    } else if( ATTEND === "出席" && ADULT === "" ){
-        document.getElementById('Adult').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請回答大人人數');
-    } else if( ATTEND === "出席" && KID === "" ){
-        document.getElementById('Kid').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請回答小孩人數');
-    } else if( ATTEND === "出席" && VEGAN === "" ){
-        document.getElementById('Vegan').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請問是否有素食需求');
-    } else if( INVITATION === "" ){
-        document.getElementById('Invitation').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請問是否需要紙本喜帖');
-    } else if( INVITATION === "需要" && ADDRESS === "" ){
-        document.getElementById('Address').scrollIntoView({ block: 'center' , behavior: 'smooth' });
-        alert('請留下喜帖收件地址');
-    } else{
-        function addZero(i) {
-            if (i < 10) {i = "0" + i}
-            return i;
-        }
         
-        const today = new Date(),
-        year = today.getFullYear(),
-        month = today.getMonth()+1,
-        day = today.getDate(),
-        h = addZero(today.getHours()),
-        m = addZero(today.getMinutes()),
-        s = addZero(today.getSeconds()),
-        time = h + ":" + m + ":" + s,
-        SUBMITTIME = year+"/"+month+"/"+day+" "+time;
-
-        document.getElementById('loading').style.display = "block";
-        
-        $.ajax({
-          type: "get",
-          url: "https://docs.google.com/spreadsheets/d/1gYgLgWY9suhtoQZQW05krghZi3G1K4xYVKasXaK1zII/edit?resourcekey=&gid=1026309814#gid=1026309814",
-          data: {
-            "SUBMITTIME": SUBMITTIME, 
-            "NAME": NAME, 
-            "ATTEND": ATTEND, 
-            "ADULT": ADULT, 
-            "KID": KID,
-            "VEGAN": VEGAN,
-            "INVITATION": INVITATION,
-            "ADDRESS": ADDRESS,
-            "MESSAGE": MESSAGE,
-          },
-          dataType: "JSON",
-          success: function(response){
-            if (response.success) {
-              submitform();
+            if( NAME === "" ){
+                document.getElementById('Name').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請填寫姓名');
+            } else if( ATTEND === "出席" && ADULT === "" ){
+                document.getElementById('Adult').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請回答大人人數');
+            } else if( ATTEND === "出席" && KID === "" ){
+                document.getElementById('Kid').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請回答小孩人數');
+            } else if( ATTEND === "出席" && VEGAN === "" ){
+                document.getElementById('Vegan').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請問是否有素食需求');
+            } else if( INVITATION === "" ){
+                document.getElementById('Invitation').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請問是否需要紙本喜帖');
+            } else if( INVITATION === "需要" && ADDRESS === "" ){
+                document.getElementById('Address').scrollIntoView({ block: 'center' , behavior: 'smooth' });
+                alert('請留下喜帖收件地址');
             } else {
-              alert("傳送異常，請再試一次");
+                submitted = true;
+                Form1.submit();
             }
-          },
-          error: function(xhr, status){
-            var errorMsg = xhr.status;
-            if(errorMsg === 0){
-                alert('異常！請再試一次');
-            } else{
-                alert (xhr.status + ':' +xhr.statusText);
-            }
-          },
-          complete: function(){
-            document.getElementById('loading').style.display = "none";
-          }
-        });
-    }
-}  
 
+        
+      
+      
+}
 function submitform(){
     const submitsuccess = document.getElementById('submit-success');
     submitsuccess.style.display = "flex";
+    document.getElementById('form1').reset();
+    AttendFunction(); // Re-run to handle display after reset
 }
 
 function submitok(){
